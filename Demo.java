80be8c540ebdfd2beea3a42ed2a2626e5dd98e0b
Venkatesh koutam <koutamvenkatesh123@gmail.com>
2026-06-26 09:42:15 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+    public static void main(String[] args) {
+        int num=5;
+        if (num %2==0) {
+            System.out.println("even number");
+
+            
+        }
+        else
+        {
+            System.out.println("odd number");
+        }
+    }
+    
+}
